b36837daa3eeadb33b8f8652f6d27511319eebf1
Mickeyyadav22 <Mickeyyadav22@users.noreply.github.com>
2024-09-20 11:28:05 +0530

    javascript conditional statements task

---
+++ evan add.js
@@ -0,0 +1,6 @@
+n1=4;
+n2=6;
+sum=n1+n2;
+if(n1%2===0 && n2%2===0)
+{console.log(sum)}
+else{console.log("invalid")}
---
+++ even odd.js
@@ -0,0 +1,8 @@
+i=25;
+if(i>0 && i%2===0)
+{console.log("positive even number")}
+ else if(i>0 && i%2!=0)
+ {console.log("positive odd number")}
+ else if (i===0)
+ {console.log("zero")}
+ else{console.log("negitive number")}
